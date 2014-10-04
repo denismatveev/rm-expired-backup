afdce1c2ce1c9dcae88c083de221ca1bcb6e71c8
dissonmatveev <denis@denis.local>
2014-10-05 02:05:53 +0600

    scandir nor done

---
+++ scandir.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "filelist.h"
+
+int scandir(int*, const char*);
+int *n;// number of elements in a directory
+
+struct dirent  *entry;
+struct stat *st;
+
+
+
+int scandir(int *n,const char *path)
+{
+
+    DIR *fd;
+
+    if((fd=opendir(path)) == NULL)
+    {
+        perror(path);
+        return 1;
+    }
+    while((entry=readdir(fd)) != NULL)
+    {
+      if(stat(dirent->d_name,st) == -1)
+      {
+          perror(dirent->d_name);
+
+          return 1;
+      }
+
+     if(S_ISDIR(st->st_mode))
+         dir_element->el_type=0;
+     else if(S_ISREG(st->st_mode))
+         dir_element->el_type=1;
+    else return -1;
+
+    
+
+
+
+
+
+    }
+
+    closedir(fd);
+
+    return 0;
+}
+   
+
