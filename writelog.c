3e2d73eeefdc1504086afadd27779fb9fd2d90fb
Denis matveev <disson@yandex.ru>
2015-02-12 16:22:24 +0500

    drafts of WriteLog() with va arguments

--- writelog.c
+++ writelog.c
@@ -9,7 +9,46 @@
     //http://stackoverflow.com/questions/327836/multi-file-c-program-how-best-to-implement-optional-logging
 */
 
-//TODO сделать функцию с переменным числом аргументов и можно будет передвать название файла и добавить loglevel(хотя есть LOG_ERR у syslog)
+//TODO сделать функцию с переменным числом аргументов и можно будет передавать название файла и добавить loglevel(хотя есть LOG_ERR у syslog)
+/*
+example:
+#define LOG_FATAL    (1)
+#define LOG_ERR      (2)
+#define LOG_WARN     (3)
+#define LOG_INFO     (4)
+#define LOG_DBG      (5)
+
+#define LOG(level, ...) do {  \
+                            if (level <= debug_level) { \
+                                fprintf(dbgstream,"%s:%d:", __FILE__, __LINE__); \
+                                fprintf(dbgstream, __VA_ARGS__); \
+                                fprintf(dbgstream, "\n"); \
+                                fflush(dbgstream); \
+                            } \
+                        } while (0)
+extern FILE *dbgstream;
+extern int  debug_level;
+
+How to use:
+
+LOG(LOG_ERR, "I/O error %s occured while opening file %s", strerror(errno), filename);
+
+*/
+/*
+
+int WriteLog(const int loglevel,const char *formatstr,...)
+{
+    
+
+
+
+}
+ 
+
+
+
+
+*/
 int WriteLog(const char *message)
 {
      syslog(LOG_WARNING, "%s%s%s",message, ": ",strerror(errno));
--- writelog.h
+++ writelog.h
@@ -1,13 +1,8 @@
 #ifndef WRITELOG_H
 #define WRITELOG_H
 
-#define LOG_FATAL    (1)
-#define LOG_ERR      (2)
-#define LOG_WARN     (3)
-#define LOG_INFO     (4)
-#define LOG_DBG      (5)
 //http://stackoverflow.com/questions/327836/multi-file-c-program-how-best-to-implement-optional-logging
-extern int loglevel;
+//int WriteLog(const int, const char*,...);
 int WriteLog(const char *);
 #endif // WRITELOG_H
 
